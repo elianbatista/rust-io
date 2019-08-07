d0265ec774d6c9e93fc9c6a4235d66e7b21d6399
Glitch (elianbatista-rust-io) <none>
2019-08-07 01:15:03 +0000

    🏋🛎 Checkpoint ./server.js:5954365/1647:5961622/1712

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+// caralho bravo entao pera ai
+//alou do vscode
+// caralho nem fudendo
+// bravo dms opa, ta ai ?
+//ta no vs? SIM auowhfwahfufo nem fudendo
+// 
+// caralho q foda 
+//mano como os cara consegue fazer mais rapido que o google docs?? nao sei asfkaksf
+// vc ve oq eu seleciono?acho que nao
+// se quiser selecionar ent s´´o mete o tab, entendi, fica meio bugado no tab se liga
+//eita
+// como q roda agr? sei la akskasfkask voce que manja ai, control shift P, show app in browser, será ?
+// tem q ver pq n ta indo o site, ta 
 
 
 server.listen(3000);
