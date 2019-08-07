b80722164e023f82dfb99504d180dc91e947ab8f
Glitch (elianbatista-rust-io) <none>
2019-08-07 01:56:51 +0000

    👐🎑 Checkpoint ./server.js:5954365/608 ./public/index.html:5954365/214 ./public/js/p5/p5.min (1).js:5954365/470927 ./public/js/p5/p5.play.js:5954365/119735

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-/*
+
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -9,32 +9,14 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 
 
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.set('views', path.join(__dirname, 'public'));
-//app.engine('html', require('ejs').renderFile);
-//app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 
 server.listen(port, function () {
     console.log('Server listening at port %d', port);
 });
 console.log("Alou");
-*/
-const express = require('express');
-const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
