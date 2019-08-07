43c4e0cc11ab4a361dbedfb888caa4e1566d110d
Glitch (elianbatista-rust-io) <none>
2019-08-07 23:28:59 +0000

    🌆📡 Checkpoint ./server.js:5961622/107

--- server.js
+++ server.js
@@ -8,15 +8,18 @@ const port = process.env.PORT || 3000;
 
 const path = require('path');
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+io.on('connect', (socket) => {
+  
+    console.log(socket.id);
+  
+});
 
 server.listen(port, function () {
     console.log('Server listening at port %d', port);
 });
-console.log("Alou");
 
