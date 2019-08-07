892db6dc7e3caa8300f46868f0ab7e6dfc1276cf
Glitch (elianbatista-rust-io) <none>
2019-08-07 23:44:07 +0000

    🌘🍤 Checkpoint ./server.js:5961622/3645 ./public/index.html:5961622/333

--- server.js
+++ server.js
@@ -13,9 +13,59 @@ app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+var arrayPlayersObject = [];
+
+var playerProt = function (x, y, id){
+  
+  this.x = x;
+  this.y = y;
+  this.id = id;
+  
+};
+
 io.on('connect', (socket) => {
   
-    console.log(socket.id);
+    var newSocket = new playerProt(Math.random()*500, Math.random()*500, socket.id);
+  
+    socket.broadcast.emit('newSocket', newSocket);
+
+    arrayPlayersObject.push(newSocket);
+
+    socket.emit('mensagem', arrayPlayersObject);
+  
+    socket.on('disconnect', function(){
+
+      socket.broadcast.emit('disconectPlayer', socket.id);
+
+      for(var i = 0; i < arrayPlayersObject.length; i++){
+
+             if(arrayPlayersObject[i]['id'] == socket.id){
+
+                    arrayPlayersObject.splice(i, 1);
+
+             }
+
+      }
+      
+    });
+  
+    socket.on('update', (playerX, playerY)=> {
+
+          for(var i = 0; i < arrayPlayersObject.length; i++){
+
+                 if(arrayPlayersObject[i]['id'] == socket.id){
+
+                        arrayPlayersObject[i]['x'] = playerX;
+
+                        arrayPlayersObject[i]['y'] = playerY;
+
+                 }
+
+                 socket.broadcast.emit('updatePositions', socket.id, playerX, playerY);
+
+          }
+
+   });
   
 });
 
