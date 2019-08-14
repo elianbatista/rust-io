15bbc96c982e190bfb3787151215e9ea6a930689
Glitch (elianbatista-rust-io) <none>
2019-08-14 19:54:17 +0000

    🌁👧 Checkpoint ./arenaServer.js:5954365/10604

--- arenaServer.js
+++ arenaServer.js
@@ -9,7 +9,8 @@ class arena {
               this.players = [];
               this.bullets = [];
               this.fruits = [];
-
+              this.quadFruit;
+         
               this.width = width;
               this.height = height;
               this.frame = 0;
@@ -167,5 +168,132 @@ function updateBullets() {
               bullet.life -= serverDeltaTime * 1000;
        }
 }
+
 */
+class quadFood {
+    constructor(center, w, h) {
+        this.fruits = [];
+        this.close = true;
+        this.leftUp = null;
+        this.leftDown = null;
+        this.rightUp = null;
+        this.rightDown = null;
+
+        this.center = center;
+        this.w = w;
+        this.h = h;
+
+
+        this.limit = 1;
+    }
+    getQuadbyPos(x, y) {
+        if (!this.close) {
+            if (x <= this.center.x) {
+                if (y <= this.center.y) {
+                    return this.leftUp.getQuadbyPos(x, y);
+                } else {
+                    return this.leftDown.getQuadbyPos(x, y);
+                }
+            } else {
+                if (y <= this.center.y) {
+                    return this.rightUp.getQuadbyPos(x, y);
+                } else {
+                    return this.rightDown.getQuadbyPos(x, y);
+
+                }
+            }
+        } else {
+            return this;
+        }
+    }
+ 
+    display() {
+        if (debugMode) {
+            noFill();
+            stroke(255, 0, 0);
+            strokeWeight(2);
+            rect(this.center.x, this.center.y, this.w, this.h)
+        }
+
+
+        if (this.fruits.length > 0) {
+            for (let food of this.fruits) {
+               // food.display();
+            }
+        }
+        if (this.leftUp) {
+            this.leftUp.display();
+        }
+        if (this.leftDown) {
+            this.leftDown.display();
+        }
+        if (this.rightUp) {
+            this.rightUp.display();
+        }
+        if (this.rightDown) {
+            this.rightDown.display();
+        }
+    }
+    createLeftUp() {
+        const center = new vec2d(this.center.x - this.w / 4, this.center.y - this.h / 4);
+        this.leftUp = new quadFood(center, this.w / 2, this.h / 2);
+    }
+    createLeftDown() {
+        const center = new vec2d(this.center.x - this.w / 4, this.center.y + this.h / 4);
+        this.leftDown = new quadFood(center, this.w / 2, this.h / 2);
+    }
+
+    createRightUp() {
+        const center = new vec2d(this.center.x + this.w / 4, this.center.y - this.h / 4);
+        this.rightUp = new quadFood(center, this.w / 2, this.h / 2);
+    }
+    createRightDown() {
+        const center = new vec2d(this.center.x + this.w / 4, this.center.y + this.h / 4);
+        this.rightDown = new quadFood(center, this.w / 2, this.h / 2);
+    }
+    createQuads() {
+        this.createLeftUp();
+        this.createLeftDown();
+        this.createRightUp();
+        this.createRightDown();
+
+        this.close = false;
+    }
+
+    checkQuad(fruit) {
+        const pos = new vec2d(fruit.pos.x, fruit.pos.y);
+
+        if (pos.x <= this.center.x) {
+            if (pos.y <= this.center.y) {
+                this.leftUp.insert(fruit)
+            } else {
+                this.leftDown.insert(fruit)
+            }
+        } else {
+            if (pos.y <= this.center.y) {
+                this.rightUp.insert(fruit)
+            } else {
+                this.rightDown.insert(fruit)
+
+            }
+        }
+
+    }
+    insert(fruit) {
+        if (this.fruits.length <= this.limit && this.close) {
+            this.fruits.push(fruit);
+        } else {
+            if (this.close) {
+                this.close = false;
+                this.createQuads();
+
+            }
+            for (let i = this.fruits.length - 1; i >= 0; i--) {
+                this.checkQuad(this.fruits[i]);
+                this.fruits.splice(i, 1);
+            }
+            this.checkQuad(fruit);
+        }
+    }
+}
 module.exports = arena;
